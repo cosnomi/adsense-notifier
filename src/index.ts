4501334500e34e1097d5e79679155c2eb88c5498
cosnomi <cosnomi@cosnomi.com>
2019-04-06 13:05:49 +0900

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
 import { google } from "googleapis";
-
-const oauth2Client = new google.auth.OAuth2(
-  "CLIENT_ID",
-  "CLIENT_SECRET",
-  "REDIRECT_URL"
-);
-const tokens = {}; // get from env var?
-const scope = "https://www.googleapis.com/auth/adsense.readonly";
-const key = "something";
-oauth2Client.setCredentials({
-  refresh_token: process.env.API_REFRESH_TOKEN
+import { oauth2 } from "googleapis/build/src/apis/oauth2";
+function getAuthenticatedOAuth2Client(
+  clientId: string,
+  clientSecret: string,
+  redirectUri: string,
+  refreshToken: string
+) {
+  const oauth2Client = new google.auth.OAuth2(
+    clientId,
+    clientSecret,
+    redirectUri
+  );
+  const tokens = {}; // get from env var?
+  const scope = "https://www.googleapis.com/auth/adsense.readonly";
+  const key = "something";
+  oauth2Client.setCredentials({
+    refresh_token: refreshToken
+  });
+  return oauth2Client;
+}
+const {
+  installed: { client_id, client_secret, redirect_uri }
+} = require("./client.json");
+const { refresh_token } = require("./token.json");
+const adsense = google.adsense({
+  version: "v1.4",
+  auth: getAuthenticatedOAuth2Client(
+    client_id,
+    client_secret,
+    redirect_uri,
+    refresh_token
+  )
 });
-const adsense = google.adsense({ version: "v1_4", auth: oauth2Client });
+console.log(adsense.adunits.get());
