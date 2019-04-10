6186e799692a3c95d799a1d426231a66faab4c26
Cosnomi <cosnomi@cosnomi.com>
2019-04-11 02:26:20 +0900

    Read config from env vars

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,21 @@ import { getAuthenticatedOAuth2Client } from "./adsense_auth";
 import { notify } from "./slack";
 
 export async function lambda_handler(event: any, context: any) {
-  const {
-    installed: { client_id, client_secret, redirect_uri }
-  } = require("./client.json");
-  const { refresh_token } = require("./token.json");
+  if (
+    !process.env.CLIENT_ID ||
+    !process.env.CLIENT_SECRET ||
+    !process.env.REDIRECT_URI
+  ) {
+    throw new Error("No client config");
+  }
+  const client_id = process.env.CLIENT_ID;
+  const client_secret = process.env.CLIENT_SECRET;
+  const redirect_uri = process.env.REDIRECT_URI;
+
+  if (!process.env.REFRESH_TOKEN) {
+    throw new Error("No REFRESH_TOKEN");
+  }
+  const refresh_token = process.env.REFRESH_TOKEN;
   const adsense = google.adsense({
     version: "v1.4",
     auth: getAuthenticatedOAuth2Client(
